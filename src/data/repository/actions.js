db9f6162bc6066bcb992cff9a3cf12c7afb98b1d
Eden Pan <teofpqy@outlook.com>
2018-06-20 10:38:31 +0800

    add fetch function.

--- src/data/repository/actions.js
+++ src/data/repository/actions.js
@@ -2,11 +2,8 @@ import fetch from 'domain/Api';
 import axios from 'axios';
 
 export const fetchRepositories = async (filter) => {
-    const res = await axios.get('/search/repositories', {
-      baseURL: 'https://api.github.com/',
-      timeout: 5000,
+    const res = fetch('/search/repositories', {
       params: filter,
-      headers: { 'Accept': 'application/vnd.github.v3+json' }
     });
-    return res.data;
+    return res;
   }
--- src/domain/Api/index.js
+++ src/domain/Api/index.js
@@ -1,20 +1,33 @@
-import axios from'axios';
+import axios from 'axios';
+import is from 'is_js';
+import invariant from 'invariant';
 
-export default fetch = (options) => {
+const instance = axios.create({
+  baseURL: 'https://api.github.com/',
+  timeout: 1000,
+  headers: { 'Accept': 'application/vnd.github.v3+json' }
+});
 
-  const instance = axios.create({
-    baseURL: 'https://api.github.com/',
-    timeout: 1000,
-    headers: {'Accept': 'application/vnd.github.v3+json'}
-  });
+export default fetch = (url, options = {}) => {
+  invariant(is.string(url), 'url must be a string');
+  invariant(is.object(options), 'options must be a plain object');
 
-  return new Promise((resolve,reject) =>{
-    instance.request(options)
-    .then((response) => {
-      resolve(response);
-    })
-    .catch((error) => {
-      reject(error);
-    });
-  });
+  const finalOptions = Object.assign({}, options, getDefaultOpts(options));
+  finalOptions.url = url;
+  return instance.request(finalOptions)
+    .then(checkResponseStatus);
+}
+
+function getDefaultOpts(options = {}) {
+  const defaultOpts = options;
+  if (is.empty(options.method) || is.falsy(options.method)) {
+    defaultOpts.method = 'GET';
+  }
+  return defaultOpts;
+}
+
+function checkResponseStatus(response) {
+  if (response.statusText.toLowerCase() == 'ok') {
+    return response.data;
+  }
 }
